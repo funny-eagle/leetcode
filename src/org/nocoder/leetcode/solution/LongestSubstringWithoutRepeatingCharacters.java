7175ef119e0a9f198a9f50b7a07faa9a4ef6cdec
yangjinlong <yangjinlong86@gmail.com>
2018-07-27 23:27:58 +0800

    3. Longest Substring Without Repeating Characters

---
+++ src/org/nocoder/leetcode/solution/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,45 @@
+package org.nocoder.leetcode.solution;
+
+import java.util.HashSet;
+
+/**
+ * 3. Longest Substring Without Repeating Characters
+ *
+ * Given a string, find the length of the longest substring without repeating characters.
+ *
+ * Examples:
+ *
+ * Given "abcabcbb", the answer is "abc", which the length is 3.
+ *
+ * Given "bbbbb", the answer is "b", with the length of 1.
+ *
+ * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ *
+ * @author jason
+ * @date 18/7/23.
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static int lengthOfLongestSubstring(String s) {
+        int res = 0, left = 0, right = 0;
+        HashSet<Character> t = new HashSet<>();
+        while (right < s.length()) {
+            if (!t.contains(s.charAt(right))) {
+                t.add(s.charAt(right++));
+                res = Math.max(res, t.size());
+            } else {
+                t.remove(s.charAt(left++));
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "abcabcbb";
+        // expect: 3, actual: 3
+        System.out.println(lengthOfLongestSubstring(s1));
+
+        String s2 = "pwwkew";
+        // expect: 3, actual: 3
+        System.out.println(lengthOfLongestSubstring(s2));
+    }
+}
