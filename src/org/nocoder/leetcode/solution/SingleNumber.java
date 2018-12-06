43280f13af0205d1c488937583247a4a894ab706
yangjinlong <yangjinlong@tzpt.com>
2018-12-06 16:11:13 +0800

    single number

---
+++ src/org/nocoder/leetcode/solution/SingleNumber.java
@@ -0,0 +1,31 @@
+package org.nocoder.leetcode.solution;
+
+/**
+ * 136. Single Number
+ * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+ *
+ * Note:
+ *
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ *
+ * Example 1:
+ *
+ * Input: [2,2,1]
+ * Output: 1
+ * Example 2:
+ *
+ * Input: [4,1,2,1,2]
+ * Output: 4
+ *
+ * @author yangjinlong
+ * @date 2018-12-06
+ */
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
