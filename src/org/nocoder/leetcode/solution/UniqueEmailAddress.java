2c0a08d1393bee6600df4df26e5687d289ccf9b8
yangjinlong <yangjinlong@tzpt.com>
2019-01-02 14:42:17 +0800

    unique email address

---
+++ src/org/nocoder/leetcode/solution/UniqueEmailAddress.java
@@ -0,0 +1,75 @@
+package org.nocoder.leetcode.solution;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+/**
+ * Every email consists of a local name and a domain name, separated by the @ sign.
+ * <p>
+ * For example, in `alice@leetcode.com`, `alice` is the local name, and `leetcode.com` is the domain name.
+ * <p>
+ * Besides lowercase letters, these emails may contain `'.'`s or `'+'`s.
+ * <p>
+ * If you add periods (`'.'`) between some characters in the **local name** part of an email address,
+ * mail sent there will be forwarded to the same address without dots in the local name.
+ * For example, `"alice.z@leetcode.com"` and `"alicez@leetcode.com"` forward to the same email address.
+ * (Note that this rule does not apply for domain names.)
+ * <p>
+ * If you add a plus (`'+'`) in the **local name**, everything after the first plus sign will be **ignored**.
+ * This allows certain emails to be filtered, for example `m.y+name@email.com` will be forwarded to `my@email.com`.
+ * (Again, this rule does not apply for domain names.)
+ * <p>
+ * It is possible to use both of these rules at the same time.
+ * <p>
+ * Given a list of `emails`, we send one email to each address in the list.
+ * How many different addresses actually receive mails?
+ * <p>
+ * <p>
+ * <p>
+ * **Example 1:**
+ * <p>
+ * ```shell
+ * Input: ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+ * Output: 2
+ * Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually receive mails
+ * ```
+ * <p>
+ * **Note:**
+ * <p>
+ * - `1 <= emails[i].length <= 100`
+ * - `1 <= emails.length <= 100`
+ * - Each `emails[i]` contains exactly one `'@'` character.
+ *
+ * @author yangjinlong
+ * @date 2019-01-02
+ */
+public class UniqueEmailAddress {
+    public int numUniqueEmails(String[] emails) {
+        if (emails == null || emails.length > 100) {
+            throw new IllegalArgumentException();
+        }
+        String pattern = "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$";
+        Set emailSet = new HashSet();
+        for (int i = 0; i < emails.length; i++) {
+            if (Pattern.matches(pattern, emails[i])) {
+                int plusIdx = emails[i].indexOf("+");
+                int atIdx = emails[i].indexOf("@");
+                String ignoreContent = emails[i].substring(plusIdx, atIdx);
+                String email = emails[i].replace(ignoreContent, "");
+                String localName = email.substring(0, email.indexOf("@"));
+                String domainName = email.substring(email.indexOf("@"), email.length() - 1);
+                email = localName.replace(".", "").concat(domainName);
+                emailSet.add(email);
+            }
+        }
+        return emailSet.size();
+    }
+
+    public static void main(String[] args) {
+        String[] emails = new String[]{"test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"};
+        UniqueEmailAddress uniqueEmailAddress = new UniqueEmailAddress();
+        int n = uniqueEmailAddress.numUniqueEmails(emails);
+        System.out.println(n);
+    }
+}
