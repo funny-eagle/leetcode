180a9fa1d7797271370fe31b9b96eddb2abd2078
杨金龙 <yangjl@travelsky.com.cn>
2023-05-19 13:03:08 +0800

    problem PlusOne

--- src/main/java/org/nocoder/leetcode/solution/PlusOne.java
+++ src/main/java/org/nocoder/leetcode/solution/PlusOne.java
@@ -9,21 +9,43 @@ package org.nocoder.leetcode.solution;
 @SuppressWarnings("ALL")
 public class PlusOne {
 
-    public static void main(String[] args) {
-
-    }
-
     public int[] plusOne(int[] digits) {
         if (digits == null || digits.length > 100) {
-            return null;
+            throw new RuntimeException("invalid parameters");
         }
-        // 8, 7, 6, 9
-        int x = digits[digits.length - 1] + 1;
-        int carry = 0;
-        if (x > 9) {
-            x = 0; //末位
-            carry = 1; // 进位
+
+        int[] res = digits;
+
+
+        int carry = 0;//进位
+        for (int i = res.length - 1; i >= 0; i--) {
+            int t;
+            if (i == res.length - 1) {
+                // 末位加1
+                t = res[i] + 1;
+            } else {
+                // 非末位加进位值
+                t = res[i] + carry;
+                carry = 0;// 重置进位值
+            }
+
+            if (t > 9) {
+                t = 0;
+                carry = 1; // 进位
+                // 如果首位是9，且进位值是1，就扩容数组，在首位补0，供进位使用
+                if (i == 0 && res[0] == 9 && carry == 1) {
+                    res = new int[res.length + 1];
+                    for (int j = 1; j < res.length; j++) {
+                        res[i] = res[j - 1];
+                    }
+                    res[0] = 1;
+                    continue;
+                }
+            }
+            res[i] = t;
         }
-        return new int[]{1, 2, 3, 4, 5, 6, 7};
+
+        return res;
     }
+
 }
--- src/test/java/com/nocoder/leetcode/solution/PlusOneTest.java
+++ src/test/java/com/nocoder/leetcode/solution/PlusOneTest.java
@@ -19,5 +19,41 @@ public class PlusOneTest {
             System.out.print(res[i] + " ");
         }
 
+        System.out.println();
+        int[] res1 = plusOne.plusOne(new int[]{8, 9, 9});
+        for (int i = 0; i < res1.length; i++) {
+            System.out.print(res1[i] + " ");
+        }
+        System.out.println();
+
+        int[] res2 = plusOne.plusOne(new int[]{9, 9});
+        for (int i = 0; i < res2.length; i++) {
+            System.out.print(res2[i] + " ");
+        }
+        System.out.println();
+
+
+
+        int[] res3 = plusOne.plusOne(new int[]{9});
+        for (int i = 0; i < res3.length; i++) {
+            System.out.print(res3[i] + " ");
+        }
+
+        System.out.println();
+
+        int[] res4 = plusOne.plusOne(new int[]{0,9});
+        for (int i = 0; i < res4.length; i++) {
+            System.out.print(res4[i] + " ");
+        }
+
+        System.out.println();
+
+        int[] res5 = plusOne.plusOne(new int[]{9, 8, 9, 9});
+        for (int i = 0; i < res5.length; i++) {
+            System.out.print(res5[i] + " ");
+        }
+        System.out.println();
+
+
     }
 }
