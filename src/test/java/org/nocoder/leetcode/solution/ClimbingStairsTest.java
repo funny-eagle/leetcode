d922bcef8977854bdf4874c289d8810d9ec9d057
杨金龙 <yangjl@travelsky.com.cn>
2023-05-23 11:04:14 +0800

    code refactor

---
+++ src/test/java/org/nocoder/leetcode/solution/ClimbingStairsTest.java
@@ -0,0 +1,23 @@
+package org.nocoder.leetcode.solution;
+
+import org.junit.Test;
+
+/**
+ * ClimbingStairs Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>5月 23, 2023</pre>
+ */
+public class ClimbingStairsTest {
+
+    /**
+     * Method: climbStairs(int n)
+     */
+    @Test
+    public void testClimbStairs() throws Exception {
+
+    }
+
+
+} 
